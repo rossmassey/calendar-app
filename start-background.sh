d4606680c275cddb77d3bba7a573dba58f7edc3c
Ross Massey <massey0ross@gmail.com>
2025-07-25 14:51:24 -0700

    update start script

--- start-background.sh
+++ start-background.sh
@@ -1,1 +1,3 @@
-uvicorn app.main:app --host 127.0.0.1 --port 8000 --workers 1 --log-level info > server.log 2>&1 &
+source venv/bin/activate
+pip install -r requirements.txt
+uvicorn app.main:app --host 127.0.0.1 --port 8000 --workers 1 --log-level info >> server.log 2>&1 &
